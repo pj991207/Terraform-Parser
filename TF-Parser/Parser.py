55a8b4aedf3ee836ea6eaa2ebf9c5daf0fd508e9
pj991207 <pj991207@gmail.com>
2022-10-25 17:36:01 +0900

    [22.10.25][add] Terraform Parser module check ( cider_block, private_subnets, public_subnets)

--- TF-Parser/Parser.py
+++ TF-Parser/Parser.py
@@ -7,7 +7,7 @@ import hcl2
 ##zip파일 열기
 def open_zip(path : str):
     openZip = zipfile.ZipFile(path)
-    openZip.extractall()
+    openZip.extractall('Example_2')
     openZip.close()
 
 ##.tf 파일 확장자를 갖는 파일의 경로를 찾음
@@ -26,23 +26,65 @@ def open_tf_file(path : str):
     find_tf_module_file(tf_dict)
 
 ##.tf 파일에서 module을 사용하는 tf 파일을 찾는다.
-def find_tf_module_file(tf_dict):
+def find_tf_module_file(tf_dict:dict):
     tf_dict_key = list(tf_dict.keys())
     module_path = []
     for key in tf_dict_key:
         if 'module' in tf_dict[key].keys():
-            #find source
             module_path.append(key)
-            #print("path : ",key)
-            #print("value : ",tf_dict[key]['module'])
-    find_tf_moudle_cidr_block(module_path)
+    find_tf_moudle(tf_dict,module_path)
 
-def find_tf_moudle_cidr_block(path):
+#module에서 cidr_block찾는다.
+def find_tf_moudle(tf_dict:dict,path:list):
+    for i in path:
+        module_dict = tf_dict[i]['module'][0]
+        module_name = list(module_dict.keys())[0]
+        module_dict_value = module_dict[module_name]
+        module_source = module_dict_value['source']
+        if type(module_source) is list:
+            module_source = find_value_in_list(module_source)
+        else:
+            pass
+        module_cidr_block = find_tf_module_cidr_block(module_dict_value,module_source)
+        module_private_subnets = find_tf_module_private_subnets(module_dict_value,module_source)
+        module_public_subnets = find_tf_module_public_subnets(module_dict_value,module_source)
+        print(i,module_cidr_block,module_private_subnets,module_public_subnets)
 
+def find_tf_module_cidr_block(module_dict_value:dict,src:str):
+    if "cidr" in module_dict_value.keys():
+        cidr_block = module_dict_value['cidr']
+        if type(cidr_block) is list:
+            cidr_block = find_value_in_list(cidr_block)
+    else:
+        pass
+    return cidr_block
 
+def find_tf_module_private_subnets(module_dict_value:dict,src:str):
+    if "private_subnets" in module_dict_value.keys():
+        private_subnets = module_dict_value['private_subnets']
+        if type(private_subnets) is list:
+            private_subnets = find_value_in_list(private_subnets)
+    else:
+        pass
+    return private_subnets
+
+def find_tf_module_public_subnets(module_dict_value:dict,src:str):
+    if "public_subnets" in module_dict_value.keys():
+        public_subnets = module_dict_value['public_subnets']
+        if type(public_subnets) is list:
+            public_subnets = find_value_in_list(public_subnets)
+    else:
+        pass
+    return public_subnets
+
+def find_value_in_list(value:list):
+    while True:
+        if type(value) == list:
+            value = value[0]
+        else:
+            return value
 
 if __name__ =="__main__":
-    name = "Example_1.zip"
+    name = "Example_2.zip"
     open_zip(name)
-    list_tf_file('Example_1')
-
+    open_tf_file('Example_2')
